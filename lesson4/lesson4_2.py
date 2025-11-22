a8283efb5ec4a3a167a7ba22a23d7b3fce731213
yujianai119 <jianai119@gmail.com>
2025-11-22 11:15:07 +0800

    modify

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -1,3 +1,18 @@
 from playwright.sync_api import sync_playwright
+from time import sleep
 
-print(type(sync_playwright()))
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        print(type(browser))
+        # 開啟新分頁
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        print(page.title())
+        sleep(5)
+        browser.close()
+        
+    print("釋放資源檔")
+
+if __name__ == "__main__":
+    main()
