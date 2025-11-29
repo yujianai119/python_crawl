2c87ac4cf19e4e5aa2f4712ee5e06d79b2bb66c8
yujianai119 <jianai119@gmail.com>
2025-11-29 09:51:05 +0800

    modify

---
+++ lesson5/lesson5_1.py
@@ -0,0 +1,26 @@
+import os
+from playwright.sync_api import sync_playwright
+
+def get_html_path()-> str:
+    """返回HTML文件的絕對路徑"""
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    html_path = os.path.join(current_dir, "waiting_demo.html")
+    return f"file://{html_path}"
+
+def main():
+    path = get_html_path()
+    with sync_playwright() as p:
+        # 啟動瀏覽器
+        browser = p.chromium.launch(headless=False, slow_mo=500)
+
+        # 打開新頁面
+        page = browser.new_page()
+
+        page.goto(path)
+
+        page.wait_for_timeout(3000)  # 等待3秒以觀察效果
+
+        browser.close()
+
+if __name__ == "__main__":
+    main()
