a6fa356ec21e55b5b3c793c9e3a6356205344ba8
yujianai119 <jianai119@gmail.com>
2025-11-29 11:50:16 +0800

    modify

--- lesson5/lesson5_1.py
+++ lesson5/lesson5_1.py
@@ -9,7 +9,6 @@ def get_html_path()-> str:
 
 def main():
     path = get_html_path()
-    print (f"Loading page from: {path}")
     with sync_playwright() as p:
         # 啟動瀏覽器
         browser = p.chromium.launch(headless=False, slow_mo=500)
@@ -21,7 +20,11 @@ def main():
 
         
         page.wait_for_load_state("networkidle")  # 等待網絡空閒
+        #page.click("#trigger-delayed")  # 點擊按鈕觸發異步操作
+        delay_button = page.locator("#trigger-delayed")  #取後Locator按鈕觸
+        delay_button.click()  #發送點擊事件
         page.wait_for_timeout(3000)  # 等待3秒以觀察效果
+
 
         browser.close()
 
